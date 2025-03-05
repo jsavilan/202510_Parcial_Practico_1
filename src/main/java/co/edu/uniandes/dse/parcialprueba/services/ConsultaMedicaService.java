afb1156f7ba82be650f145d8177010a170a8251e
jsavilan <js.avilan@uniandes.edu.co>
2025-03-05 08:49:11 -0500

    Servicios de las entidades terminadas

--- src/main/java/co/edu/uniandes/dse/parcialprueba/services/ConsultaMedicaService.java
+++ src/main/java/co/edu/uniandes/dse/parcialprueba/services/ConsultaMedicaService.java
@@ -1,5 +1,38 @@
 package co.edu.uniandes.dse.parcialprueba.services;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.edu.uniandes.dse.parcialprueba.entities.ConsultaMedicaEntity;
+import co.edu.uniandes.dse.parcialprueba.exceptions.IllegalOperationException;
+import co.edu.uniandes.dse.parcialprueba.repositories.ConsultaMedicaRepository;
+import jakarta.transaction.Transactional;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Data
+@Service
 public class ConsultaMedicaService {
+
+    @Autowired
+    private ConsultaMedicaRepository consultaMedicaRepository;
+
+    @Transactional
+    public ConsultaMedicaEntity createConsultaMedica(ConsultaMedicaEntity consultaMedica) throws IllegalOperationException {
+        log.info("Inicia proceso de creación de una consulta medica");
+
+        if (!(consultaMedica.getFecha().after(new Date()))) {
+            throw new IllegalOperationException("La fecha de la consulta medica debe ser posterior a la fecha actual");
+        }
+
+        ConsultaMedicaEntity consultaMedicaNueva = consultaMedicaRepository.save(consultaMedica);
+
+        log.info("Finaliza proceso de creación de una consulta medica");
+
+        return consultaMedicaNueva;
+    }
 
 }
--- src/main/java/co/edu/uniandes/dse/parcialprueba/services/PacienteService.java
+++ src/main/java/co/edu/uniandes/dse/parcialprueba/services/PacienteService.java
@@ -1,5 +1,31 @@
 package co.edu.uniandes.dse.parcialprueba.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.edu.uniandes.dse.parcialprueba.entities.PacienteEntity;
+import co.edu.uniandes.dse.parcialprueba.repositories.PacienteRepository;
+import jakarta.transaction.Transactional;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Data
+@Service
 public class PacienteService {
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    @Transactional
+    public PacienteEntity createPaciente(PacienteEntity paciente) {
+        log.info("Inicia proceso de creación de un paciente");
+
+        PacienteEntity pacienteNuevo = pacienteRepository.save(paciente);
+
+        log.info("Finaliza proceso de creación de un paciente");
+
+        return pacienteNuevo;
+    }
 
 }
